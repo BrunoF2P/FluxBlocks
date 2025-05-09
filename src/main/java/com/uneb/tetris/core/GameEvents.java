38f6fcf47258db6122a59e2cb493ab38ef21ed95
BrunoF2P <brunobispof2p@gmail.com>
2025-05-08 23:34:49 -0300

    adicionando eventos de menu do jogo

--- src/main/java/com/uneb/tetris/core/GameEvents.java
+++ src/main/java/com/uneb/tetris/core/GameEvents.java
@@ -5,7 +5,10 @@ import com.uneb.tetris.event.EventType;
 public final class GameEvents {
     public static final class UiEvents {
         public static final EventType<Void> START = new EventType<Void>() {};
-        public static final EventType<Void> PLAY_BUTTON_CLICKED = new EventType<Void>() {} ;
+        public static final EventType<Void> PLAY_GAME =  new EventType<Void>() {};
+        public static final EventType<Void> OPTIONS =  new EventType<Void>() {};
+        public static final EventType<Void> RANKING =  new EventType<Void>() {};
+
     }
     public static final class GameplayEvents {
         public static final EventType<Integer> LINE_CLEARED = new EventType<Integer>() {};
