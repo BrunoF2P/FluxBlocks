3f4408245edfc8c6d6554fe52bd9055c74edbfa5
BrunoF2P <brunobispof2p@gmail.com>
2025-05-08 23:33:53 -0300

    configurando module java para o bendito funcionar com importacoes de assets

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,16 @@
-module com.uneb.tetris {
+open module com.uneb.tetris {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.graphics;
 
     requires com.almasb.fxgl.all;
 
-    opens com.uneb.tetris to javafx.fxml;
     exports com.uneb.tetris;
+    exports com.uneb.tetris.core;
+    exports com.uneb.tetris.ui;
+    exports com.uneb.tetris.ui.components;
+    exports com.uneb.tetris.ui.screens;
+    exports com.uneb.tetris.event;
+
 }
