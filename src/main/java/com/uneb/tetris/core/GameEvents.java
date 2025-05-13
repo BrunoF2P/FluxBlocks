d339cb01cec1e291c4aa4c8d11ecf18ec9cc0bbc
BrunoF2P <brunobispof2p@gmail.com>
2025-05-13 00:35:32 -0300

    UiEvents foram convertidos para uma classe abstrata e InputEvents adicionados para melhor organização. Novos tipos de eventos foram introduzidos para lidar com vários estados e ações do jogo, como atualizações do tabuleiro, alterações de pontuação e redefinições de entrada. Melhoria na manutenibilidade e escalabilidade do tratamento de eventos no núcleo do jogo.

--- src/main/java/com/uneb/tetris/core/GameEvents.java
+++ src/main/java/com/uneb/tetris/core/GameEvents.java
@@ -1,18 +1,41 @@
 package com.uneb.tetris.core;
 
 import com.uneb.tetris.event.EventType;
+import com.uneb.tetris.piece.Tetromino;
 
 public final class GameEvents {
-    public static final class UiEvents {
+    public static abstract class UiEvents {
+        private UiEvents() {}
         public static final EventType<Void> START = new EventType<Void>() {};
-        public static final EventType<Void> PLAY_GAME =  new EventType<Void>() {};
-        public static final EventType<Void> OPTIONS =  new EventType<Void>() {};
-        public static final EventType<Void> RANKING =  new EventType<Void>() {};
-
+        public static final EventType<Void> PLAY_GAME = new EventType<Void>() {};
+        public static final EventType<Void> OPTIONS = new EventType<Void>() {};
+        public static final EventType<Void> RANKING = new EventType<Void>() {};
+        public static final EventType<int[][]> BOARD_UPDATE = new EventType<>() {};
+        public static final EventType<Tetromino> NEXT_PIECE_UPDATE = new EventType<>() {};
+        public static final EventType<Integer> SCORE_UPDATE = new EventType<>() {};
+        public static final EventType<Integer> LEVEL_UPDATE = new EventType<>() {};
+        public static final EventType<String> TIME_UPDATE = new EventType<>() {};
+        public static final EventType<Void> GAME_STARTED = new EventType<>() {};
+        public static final EventType<Boolean> GAME_PAUSED = new EventType<>() {};
+        public static final EventType<Integer> GAME_OVER = new EventType<>() {};
     }
     public static final class GameplayEvents {
-        public static final EventType<Integer> LINE_CLEARED = new EventType<Integer>() {};
+        private GameplayEvents() {}
+        public static final EventType<Integer> LINE_CLEARED = new EventType<>() {};
+        public static final EventType<Void> MOVE_LEFT = new EventType<>() {};
+        public static final EventType<Void> MOVE_RIGHT = new EventType<>() {};
+        public static final EventType<Void> MOVE_DOWN = new EventType<>() {};
+        public static final EventType<Void> ROTATE = new EventType<>() {};
+        public static final EventType<Void> DROP = new EventType<>() {};
+        public static final EventType<Void> GAME_OVER = new EventType<>() {};
+        public static final EventType<Void> PAUSE = new EventType<>() {};
+        public static final EventType<Integer> SCORE_UPDATED = new EventType<>() {};
     }
 
-    private GameEvents(){}
+    public static final class InputEvents {
+        private InputEvents() {}
+        public static final EventType<String> ROTATE_RESET = new EventType<>() {};
+    }
+
+    private GameEvents() {}
 }
